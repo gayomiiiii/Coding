3ab52483e9cd1681686154f63e25bb94bf581a6f
최가영 <yh460700@naver.com>
2023-08-04 08:56:57 +0900

    [Bronze III] Title: 오븐 시계, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.py
@@ -0,0 +1,13 @@
+a, b = map(int, input().split())
+c = int(input())
+
+hour = (b+c)//60
+min = (b+c)%60
+
+if (b+c) >= 60:
+    if a+hour >= 24:
+        a = a-24
+    a = a+hour
+    print(a, min)
+else:
+    print(a, b+c)
