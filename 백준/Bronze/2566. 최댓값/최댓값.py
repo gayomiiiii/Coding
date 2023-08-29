c3825bdb01ab2d99d3d7262e239a699f9c4d4a08
최가영 <yh460700@naver.com>
2023-08-29 16:31:40 +0900

    [Bronze III] Title: 최댓값, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.py
+++ 백준/Bronze/2566. 최댓값/최댓값.py
@@ -1,15 +1,8 @@
 arr = [list(map(int, input().split())) for _ in range(9)]
 
 max_val = max(map(max, arr))
-max_row, max_col = 0, 0
-
-# 최댓값의 위치 찾기
-for i in range(9):
-    for j in range(9):
-        if arr[i][j] == max_val:
-            max_row = i+1
-            max_col = j+1
-            break
-
+max_list = [(i+1,j+1) for i in range(9) for j in range(9) if arr[i][j] == max_val]
 print(max_val)
-print(max_row, max_col)
+
+for row in max_list:
+    print(*row)
