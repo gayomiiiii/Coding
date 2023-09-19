39f66b3a177b880f49bf7b3dbf3318c79e27f11d
최가영 <yh460700@naver.com>
2023-09-20 08:57:15 +0900

    [Silver V] Title: 영화감독 숌, Time: 620 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -0,0 +1,13 @@
+# 1436 영화감독 숌
+
+n = int(input())
+world = 666
+count = 0
+
+while True:
+    if '666' in str(world):
+        count += 1
+    if count == n:
+        print(world)
+        break
+    world += 1
