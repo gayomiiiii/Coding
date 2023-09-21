f4ff39228615d81058aebb5e20f8f10551087dc3
최가영 <yh460700@naver.com>
2023-09-21 09:23:03 +0900

    [Silver IV] Title: 괄호, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -0,0 +1,21 @@
+# 9012번 - 괄호
+
+t = int(input())
+
+for i in range(t):
+    stack = []
+    a = input()
+    for j in a:
+        if j == '(':
+            stack.append(j)
+        elif j ==')':
+            if stack:
+                stack.pop()
+            else:
+                print('NO')
+                break
+    else:
+        if not stack:
+            print('YES')
+        else:
+            print('NO')
