81e86b2fb5e166dde22e88d909fcb5c243d3a965
최가영 <yh460700@naver.com>
2023-10-16 16:36:33 +0900

    [level unrated] Title: 약수 구하기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120897. 약수 구하기/약수 구하기.py
+++ 프로그래머스/unrated/120897. 약수 구하기/약수 구하기.py
@@ -1,8 +1,3 @@
 def solution(n):
-    answer = []
-    for i in range(1, n+1):
-        if n%i == 0:
-            answer.append(i)
-        else:
-            continue
+    answer = [i for i in range(1, n+1) if n%i == 0]
     return sorted(answer, reverse=False)
