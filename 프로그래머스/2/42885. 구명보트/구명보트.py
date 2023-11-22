fe9455302d051169b6115fabe67d54d25df13fd0
최가영 <yh460700@naver.com>
2023-11-22 17:20:56 +0900

    [level 2] Title: 구명보트, Time: 7.27 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -0,0 +1,14 @@
+def solution(people, limit):
+    answer = 0
+    people.sort()
+    
+    a = 0
+    b = len(people)-1
+    
+    while a < b:
+        if people[a] + people[b] <= limit:
+            answer += 1
+            a += 1
+        b -= 1
+            
+    return len(people) - answer
